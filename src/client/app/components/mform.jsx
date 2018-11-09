e82ded51e9791c2a623ac597579c5c96f8037d1b
myxomor <gr981@yandex.ru>
2018-11-09 21:58:35 +0300

    translation from INPUT to DIV

--- src/client/app/components/mform.jsx
+++ src/client/app/components/mform.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 class MForm extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            value: "Hi users!"
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+    };
 
     render () {
         return (
             <div>
-                MForm
+                <form>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}>
+                    </input>
+                </form>
+                <div>
+                    {this.state.value}
+                </div>
             </div>);
     }
 
